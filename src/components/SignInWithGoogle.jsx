b8188767f9d75d7280b4d138c37297ddbf0db183
madsnbkristensen <madskristensen@hotmail.dk>
2023-10-24 21:58:49 +0200

    google sign in - local storage

--- src/components/SignInWithGoogle.jsx
+++ src/components/SignInWithGoogle.jsx
@@ -5,51 +5,90 @@ import { auth, firestore } from '../firebaseConfig';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import GoogleIcon from '../assets/images/googleicon.svg';
+import { useEffect, useState} from 'react';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { app } from '../firebaseConfig';
 
 
 
 const GoogleSignInButton = () => {
+
   const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
 
-const handleSignIn = async (e) => {
-  e.preventDefault();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const firestore = getFirestore(app);
+      const usersCollection = collection(firestore, 'users');
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
+      try {
+        const querySnapshot = await getDocs(usersCollection);
 
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
+        const usersData = [];
+        querySnapshot.forEach((doc) => {
+          const userData = doc.data();
+          usersData.push(userData);
+        });
 
-    let firstName = '';
-    let lastName = '';
+        setUsers(usersData);
 
-    if (user.displayName) {
-      const nameParts = user.displayName.split(' ');
-      firstName = nameParts[0];
-      lastName = nameParts.slice(1).join(' ');
-    }
+        console.log(usersData);
 
-    const userRef = doc(firestore, 'users', user.uid);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: 'select_account' });
 
     try {
-      const userSnapshot = await getDoc(userRef);
-    
-    
-      if (!userSnapshot.exists()) {
-        navigate('/signup', {
-          state: { email: user.email, firstName: firstName, lastName: lastName },
-        });
-      } else {
-        navigate('/home');
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      let firstName = '';
+      let lastName = '';
+
+      if (user.displayName) {
+        const nameParts = user.displayName.split(' ');
+        firstName = nameParts[0];
+        lastName = nameParts.slice(1).join(' ');
+      }
+
+      const userRef = doc(firestore, 'users', user.uid);
+
+      try {
+        const userSnapshot = await getDoc(userRef);
+
+
+        if (!userSnapshot.exists()) {
+          navigate('/signup', {
+            state: { email: user.email, firstName: firstName, lastName: lastName },
+          });
+        } else {
+
+          const foundUser = users.find(user => user.email === user.email);
+
+          if (foundUser) {
+            // Store the user's data in local storage
+            sessionStorage.setItem('userData', JSON.stringify(foundUser));
+            console.log('User data stored in local storage:', foundUser);
+          }
+          navigate('/home');
+        }
+      } catch (error) {
+        console.error('Error getting user snapshot:', error);
       }
     } catch (error) {
-      console.error('Error getting user snapshot:', error);
+      console.error('Error signing in with Google:', error);
     }
-  } catch (error) {
-    console.error('Error signing in with Google:', error);
-  }
-};
+  };
 
   return (
     <div className='buttonWrapper'>
