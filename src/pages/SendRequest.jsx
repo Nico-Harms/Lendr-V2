b763e4155e3956197aa39f6615ebaa080d1e9cf6
madsnbkristensen <madskristensen@hotmail.dk>
2023-10-26 15:24:48 +0200

    dd

--- src/pages/SendRequest.jsx
+++ src/pages/SendRequest.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'; // Import useParams
 import { X, ArrowLeft } from "@phosphor-icons/react";
+import "../pages/pageCss/SendRequest.css";
 
 export default function SendRequest() {
 
@@ -28,17 +29,45 @@ export default function SendRequest() {
 
     return (
 
-        <main>
-            <div className="arrowWrapper">
-                <ArrowLeft size={32} /><X size={32} />
+        <main className="sendRequestMain">
+            <div className="sendRequestWrapper">
+                <div className="arrowWrapper">
+                    <ArrowLeft size={32} /><X size={32} />
+                </div>
+                <section className="rentPeriodWrapper">
+                    <div className="periodInfo">
+                        <h3>Lejeperiode</h3>
+                        <input type="date" name="" id="" />
+                        <p>Aktuel pris:</p> <span></span>
+                        <p>Depositum:</p> <span></span>
+                    </div>
+                    <div className="rentPeriodImgWrapper"><img src={post.image} alt="" />
+                    </div>
+                </section>
+                <section className="productInfoWrapper">
+                    <h3>Produktinfo</h3>
+                    <div className="productInfo">
+                        <div>
+                            <p>Str.</p>
+                            <p>{post.details.size}</p>
+                        </div>
+                        <div>
+                            <p>Mærke.</p>
+                            <p>{post.title}</p>
+                        </div>
+                        <div>
+                            <p>Tilstand</p>
+                            <p>{post.details.quality}</p>
+                        </div>
+                        <div>
+                            <div>Lokation</div>
+                            <p>{post.address}</p>
+                        </div>
+                    </div>
+                </section>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <button>Send anmodning</button>
             </div>
-            <section className="rentPeriod">
-                <h3>Lejeperiode</h3>
-                <input type="date" name="" id="" />
-                <p>Aktuel pris:</p> <span></span>
-                <p>Depositum:</p> <span></span>
-            </section>
-            
-        </main>
+        </main >
     )
 }
