784023aef90ed6d623312c9a62732dc329646c46
TobiasRW <tobiasrw98@gmail.com>
2023-10-26 12:30:27 +0200

    accepted deal page s

--- src/pages/AcceptedDeal.jsx
+++ src/pages/AcceptedDeal.jsx
@@ -1,9 +1,19 @@
-
+import "../pages/pageCss/AcceptedDeal.css"
+import mascot from "../assets/images/maskot.svg"
 
 export default function AcceptedDeal() {
-    return(
-        <div>
-            <h1>AcceptedDeal</h1>
-        </div>
+    return (
+        <main>
+            <div className="accpted-deal-wrapper">
+                <p>Tillykke med din nye aftle!</p>
+                <div className="l-quote">
+                    <div className="quote">
+                        <p>Du har netop sparet planeten for ca. 20 kg CO2-udledning ved at udleje din kuffert i stedet for at der skal produceres en ny!</p>
+                    </div>
+                </div>
+                <img src={mascot} alt="maskot" className="mascot-accept" />
+            </div>
+
+        </main>
     )
 }
