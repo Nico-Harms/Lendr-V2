ecca686ec393bab821fa722d65323c9d9f9cdf77
madsnbkristensen <madskristensen@hotmail.dk>
2023-10-26 10:03:20 +0200

    dd

---
+++ src/pages/ChooseCategory.jsx
@@ -0,0 +1,21 @@
+export default function ChooseCategory() {
+    return (
+        <main>
+            <h2>Hvad søger du?</h2>
+            <div className="categoryOptionWrapper">
+                <img src="" alt="" />
+                <h4>Kufferter</h4>
+            </div>
+            <div className="categoryOptionWrapper">
+                <img src="" alt="" />
+                <h4>Rygsække</h4>
+            </div>
+            <div className="categoryOptionWrapper">
+                <img src="" alt="" />
+                <h4>Tilbehør</h4>
+            </div>
+
+            <p>Vis alle produkter</p>
+        </main>
+    );
+}
